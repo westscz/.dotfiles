52bab07b9aee73360f4743a00e1764b537f01e76
Jaroslaw Piszczala <(none)>
2020-03-09 18:36:26 +0100

    Update command to fix LVDS resolution

--- scripts/change_video_output.sh
+++ scripts/change_video_output.sh
@@ -5,7 +5,7 @@ HDMI="$(xrandr | grep -e 'HDMI-[0-9]* connected')"
 if [ -n "$VGA" ]
 then
 	notify-send 'VGA connected'
-	xrandr --output VGA-1 --auto --right-of LVDS-1
+	xrandr --output LVDS-1 --mode 1366x768 --output VGA-1 --auto --right-of LVDS-1
 	pacmd set-card-profile alsa_card.pci-0000_00_1b.0 output:analog-stereo
 fi
 
