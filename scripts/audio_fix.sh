fc765153c324af8c79b6c573382559e40534f8dc
Jaroslaw Piszczala <(none)>
2022-02-23 19:46:35 +0100

    push before local files removal

---
+++ scripts/audio_fix.sh
@@ -0,0 +1,4 @@
+# https://askubuntu.com/questions/951016/fuzzy-garbled-audio-with-slight-echo
+# https://bugs.launchpad.net/ubuntu/+source/speech-dispatcher/+bug/1736222
+
+sudo apt remove speech-dispatcher
