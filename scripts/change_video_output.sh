1ff682a2a53eed47d05806ea51d133f857b1b499
Jaroslaw Piszczala <(none)>
2022-02-23 19:46:21 +0100

    push before local files removal

--- scripts/change_video_output.sh
+++ scripts/change_video_output.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
-set -euo pipefail
+#set -euo pipefail
 
 VGA="$(xrandr | grep -e 'VGA-[0-9]* connected')"
 HDMI="$(xrandr | grep -e 'HDMI-[0-9]* connected')"
 
 if [ -n "$VGA" ]
 then
-	notify-send 'VGA connected'
+	echo 'VGA connected'
 	xrandr --output LVDS-1 --mode 1366x768 --output VGA-1 --auto --right-of LVDS-1
 	pacmd set-card-profile alsa_card.pci-0000_00_1b.0 output:analog-stereo
 fi
 
 if [ -n "$HDMI" ]
 then
-	notify-send 'HDMI connected'
+	echo 'HDMI connected'
 	xrandr --output HDMI-1 --auto --right-of LVDS-1
 	pacmd set-card-profile alsa_card.pci-0000_00_1b.0 output:hdmi-stereo
 fi
 
 if [ "$VGA" = "$HDMI" ]
 then
+    echo 'Disconnected'
 	notify-send 'Disconnected'
 	pacmd set-card-profile alsa_card.pci-0000_00_1b.0 output:analog-stereo
 fi
